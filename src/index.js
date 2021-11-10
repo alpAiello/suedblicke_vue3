b3e457433e3eec6717d233c82615d19b737861b8
alpAiello <4lessandro.aiello@gmail.com>
2021-11-10 11:59:42 +0100

    added eslint prettier compatibility

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import _ from "lodash";
-import "./style.scss";
+import _ from 'lodash';
+import './style.scss';
 
 function component() {
-  const element = document.createElement("div");
-  const innerElement = document.createElement("div");
-  const array = [{ value: 1 }, { value: 2 }, { value: 3 }, { value: 4 }];
+  const element = document.createElement('div');
+  const innerElement = document.createElement('div');
+  const array = [
+    { value: 1 },
+    { value: 2 },
+    { value: 3 },
+    { value: 4 },
+  ];
   const billo = (array) => array[0];
   console.log(billo(array));
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-  innerElement.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  innerElement.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.append(innerElement);
-  innerElement.classList.add("hello2");
+  innerElement.classList.add('hello2');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          'style-loader',
           // Translates CSS into CommonJS
-          "css-loader",
+          'css-loader',
           // Compiles Sass to CSS
-          "sass-loader",
+          'sass-loader',
         ],
       },
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"],
+        use: ['babel-loader', 'eslint-loader'],
       },
     ],
   },
   resolve: {
-    extensions: ["*", ".js"],
+    extensions: ['*', '.js'],
   },
 };
